0794fcebd39a466fd9ea95eb65ab641635875982
Qloudy <117589336+Q-loudy@users.noreply.github.com>
2025-06-06 21:32:59 +0800

    Update reality.js - rm sqrt from globalspeedfactor

--- src/core/reality.js
+++ src/core/reality.js
@@ -130,7 +130,7 @@ export function simulatedRealityCount(advancePartSimCounters) {
   if (advancePartSimCounters) {
     player.partSimulatedReality = simCount - Math.floor(simCount);
   }
-  return Math.floor(simCount * Math.sqrt(globalSpeedFactor));
+  return Math.floor(simCount * globalSpeedFactor);
 }
 
 /**
