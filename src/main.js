263dc93e63fa925c331671c30aee797e71d04235
Qloudy <117589336+Q-loudy@users.noreply.github.com>
2025-08-08 05:38:18 +0800

    Update main.js - import achievements?

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import "./merge-globals";
 import { browserCheck, init } from "./game";
 import { DEV } from "./env";
 import { watchLatestCommit } from "./commit-watcher";
+import { Achievements } from './core/achievements/normal-achievement.js';
+
+window.Achievements = Achievements;
 
 if (browserCheck()) init();
 if (DEV) watchLatestCommit();
