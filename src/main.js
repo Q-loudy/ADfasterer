187953bd9b281f8cbb49f46d4299b32a51976bc2
Qloudy <117589336+Q-loudy@users.noreply.github.com>
2025-08-08 05:43:31 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import "./merge-globals";
 import { browserCheck, init } from "./game";
 import { DEV } from "./env";
 import { watchLatestCommit } from "./commit-watcher";
-import { Achievements } from './core/achievements/normal-achievement.js';
-
-window.Achievements = Achievements;
 
 if (browserCheck()) init();
 if (DEV) watchLatestCommit();
